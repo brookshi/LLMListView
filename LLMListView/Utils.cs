1ada71ce0744e42f9dc789b5172198099b30dcee
brookshi <iwxiaot@gmail.com>
2015-09-25 17:53:57 +0800

    anim when completed swipe

---
+++ LLMListView/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace LLM
+{
+    public class Utils
+    {
+        public static DoubleAnimation CreateDoubleAnimation(DependencyObject target, string propertyPath, EasingFunctionBase easingFunc, double to, double duration)
+        {
+            var anim = new DoubleAnimation()
+            {
+                To = to,
+                Duration = new Duration(TimeSpan.FromMilliseconds(duration)),
+                EasingFunction = easingFunc
+            };
+            Storyboard.SetTarget(anim, target);
+            Storyboard.SetTargetProperty(anim, propertyPath);
+            return anim;
+        }
+    }
+}
