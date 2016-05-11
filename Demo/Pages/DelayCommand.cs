e9ddb0150f683b3d0d7e7d560e12a4e7c89bd859
brookshi <iwxiaot@gmail.com>
2016-05-11 20:53:05 +0800

    add template with command in code behind

---
+++ Demo/Pages/DelayCommand.cs
@@ -0,0 +1,78 @@
+﻿#region License
+//   Copyright 2015 Brook Shi
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License. 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Demo.Pages
+{
+    public class DelayCommand<T> : ICommand
+    {
+        private readonly Func<T, bool> _canExecuteMethod;
+        private readonly Action<T> _executeMethod;
+
+        public DelayCommand(Action<T> executeMethod)
+            : this(executeMethod, null)
+        {
+        }
+
+        public DelayCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            try
+            {
+                return CanExecute((T)parameter);
+            }
+            catch { return false; }
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute((T)parameter);
+        }
+
+        public bool CanExecute(T parameter)
+        {
+            return ((_canExecuteMethod == null) || _canExecuteMethod(parameter));
+        }
+
+        public void Execute(T parameter)
+        {
+            _executeMethod?.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged(EventArgs.Empty);
+        }
+
+        protected virtual void OnCanExecuteChanged(EventArgs e)
+        {
+            CanExecuteChanged?.Invoke(this, e);
+        }
+    }
+}
